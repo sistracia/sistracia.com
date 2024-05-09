ba60eff59c5fad01d1ab949a1b5246164fc704ed
sistracia <156994197+sistracia@users.noreply.github.com>
2024-05-09 17:20:40 +0700

    Update post's detail page

--- Program.fs
+++ Program.fs
@@ -5,6 +5,7 @@ open Falco.Routing
 open Falco.HostBuilder
 open Falco.Markup
 open Markdig
+open Markdig.Prism
 open HtmlAgilityPack
 open Microsoft.AspNetCore.Http
 open System
@@ -13,6 +14,10 @@ open GrayMattr
 
 let contentPath: string = "./content"
 let contentExt: string = "*.md"
+
+let githubURL: string = "https://github.com/sistracia/sistracia.com"
+let githubContentOriginURL: string = $"{githubURL}/blob/main/content"
+let githubContentHistoryURL: string = $"{githubURL}/commits/main/content"
 
 [<CLIMutable>]
 type Mattr =
@@ -25,19 +30,53 @@ type Mattr =
 type FileMeta =
     { Slug: string
       CreationTime: DateTime
-      ModificationType: DateTime
+      ModificationTime: DateTime
       Content: string
       Mattr: Mattr }
+
+    member this.FormattedCreationTime = this.CreationTime.ToString("d.M.yyyy")
+    member this.FormattedModificationTime = this.ModificationTime.ToString("d.M.yyyy")
 
 [<Struct>]
 type FileResponse =
     { CreationTime: DateTime
-      ModificationType: DateTime
+      ModificationTime: DateTime
       Slug: string
       Mattr: Mattr }
 
 module Utils =
-    let mdToHtml (md: string) : string = Markdown.ToHtml md
+    let markdigPipeline: MarkdownPipeline =
+        (new MarkdownPipelineBuilder())
+            .UseAbbreviations()
+            .UseAdvancedExtensions()
+            .UseAutoIdentifiers()
+            .UseAutoLinks()
+            .UseBootstrap()
+            .UseCitations()
+            .UseCustomContainers()
+            .UseDefinitionLists()
+            .UseDiagrams()
+            .UseEmojiAndSmiley()
+            .UseEmphasisExtras()
+            .UseFigures()
+            .UseFooters()
+            .UseFootnotes()
+            .UseGenericAttributes()
+            .UseGlobalization()
+            .UseListExtras()
+            .UseMathematics()
+            .UseMediaLinks()
+            .UseNonAsciiNoEscape()
+            .UsePipeTables()
+            .UsePragmaLines()
+            .UsePreciseSourceLocation()
+            .UseReferralLinks()
+            .UseTaskLists()
+            .UseYamlFrontMatter()
+            .UsePrism()
+            .Build()
+
+    let mdToHtml (md: string) : string = Markdown.ToHtml(md, markdigPipeline)
 
     let htmlToHtmlDoc (html: string) : HtmlDocument =
         let doc: HtmlDocument = new HtmlDocument()
@@ -77,11 +116,17 @@ module Utils =
 
     let mdToHtmlDoc = mdToHtml >> htmlToHtmlDoc
 
-    let limitFileMetas (limit: int) (fileMetas: FileMeta seq) =
+    let limitFileMetas (limit: int) (fileMetas: FileMeta seq) : FileMeta seq =
         if limit <= 0 || limit > (fileMetas |> Seq.length) then
             fileMetas
         else
             fileMetas |> Seq.take limit
+
+    let contentOrigin (slug: string) : string =
+        $"{githubContentOriginURL}/{Uri.EscapeDataString(slug)}.md"
+
+    let contentHistory (slug: string) : string =
+        $"{githubContentHistoryURL}/{Uri.EscapeDataString(slug)}.md"
 
 module Reader =
     let loadFiles (contentPath: string) (contentExt: string) : Async<FileMeta seq> =
@@ -93,7 +138,7 @@ module Reader =
 
                     { FileMeta.Slug = Path.GetFileNameWithoutExtension filePath
                       FileMeta.CreationTime = File.GetCreationTime filePath
-                      FileMeta.ModificationType = File.GetLastWriteTime filePath
+                      FileMeta.ModificationTime = File.GetLastWriteTime filePath
                       Content = fileMattr.Content
                       FileMeta.Mattr =
                         match fileMattr.Data with
@@ -103,16 +148,13 @@ module Reader =
 
 module Content =
 
-    let getHtml (fileMeta: FileMeta) : Async<string> =
+    let getHtml (fileMeta: FileMeta) : string = Utils.mdToHtml fileMeta.Content
 
-
-        async { return Utils.mdToHtml fileMeta.Content }
-
-    let getJson (fileMeta: FileMeta) : Async<obj> =
-        async { return Utils.getHtmlBodyElements ((Utils.mdToHtmlDoc fileMeta.Content).DocumentNode) }
+    let getJson (fileMeta: FileMeta) : obj =
+        Utils.getHtmlBodyElements ((Utils.mdToHtmlDoc fileMeta.Content).DocumentNode)
 
 module HtmlView =
-    let master (title: string) (styleLink: string) (content: XmlNode list) : XmlNode =
+    let master (title: string) (styleLinks: string array) (content: XmlNode list) : XmlNode =
         Elem.html
             [ Attr.lang "en" ]
             [ Elem.head
@@ -172,7 +214,9 @@ module HtmlView =
                                             isolation: isolate;
                                         }
                         """ ]
-                    Elem.link [ Attr.href styleLink; Attr.rel "stylesheet" ] ]
+                    yield!
+                        [ for styleLink: string in styleLinks do
+                              Elem.link [ Attr.href styleLink; Attr.rel "stylesheet" ] ] ]
               Elem.body [] content ]
 
     let postCard (fileMeta: FileMeta) : XmlNode =
@@ -183,13 +227,13 @@ module HtmlView =
                   [ Elem.a
                         [ Attr.href (sprintf $"{Uri.EscapeDataString(fileMeta.Slug)}") ]
                         [ Text.raw fileMeta.Mattr.Title ] ]
-              Elem.span [] [ Text.raw (fileMeta.CreationTime.ToString("d.M.yyyy")) ]
-              Elem.p [] [ Text.raw (fileMeta.Mattr.Description) ] ]
+              Elem.span [] [ Text.raw fileMeta.FormattedCreationTime ]
+              Elem.p [] [ Text.raw fileMeta.Mattr.Description ] ]
 
     let homeView (fileMetas: FileMeta seq) : XmlNode =
         master
             "Writting"
-            "home.css"
+            [| "styles/home.css" |]
             [ Elem.div
                   [ Attr.id "root" ]
                   [ Elem.header
@@ -209,23 +253,59 @@ module HtmlView =
                                     [ for fileMeta: FileMeta in fileMetas do
                                           postCard fileMeta ] ] ] ] ]
 
-    let detailView (content: string) : XmlNode =
-        master "Writting" "home.css" [ Elem.main [] [ Text.raw content ] ]
+    let detailView (content: string) (fileMeta: FileMeta) : XmlNode =
+        master
+            "Writting"
+            [| "styles/detail.css"; "styles/prism.css" |]
+            [ Elem.div
+                  [ Attr.id "root" ]
+                  [ Elem.header
+                        []
+                        [ Elem.h1
+                              []
+                              [ Elem.a
+                                    [ Attr.href (Utils.contentOrigin fileMeta.Slug) ]
+                                    [ Text.raw fileMeta.Mattr.Title ] ]
+                          Elem.table
+                              []
+                              [ Elem.tbody
+                                    []
+                                    [ Elem.tr
+                                          []
+                                          [ Elem.td [] [ Text.raw "Created" ]
+                                            Elem.td
+                                                []
+                                                [ Elem.a
+                                                      [ Attr.href (Utils.contentOrigin fileMeta.Slug) ]
+                                                      [ Text.raw fileMeta.FormattedCreationTime ] ] ]
+                                      Elem.tr
+                                          []
+                                          [ Elem.td [] [ Text.raw "Updated" ]
+                                            Elem.td
+                                                []
+                                                [ Elem.a
+                                                      [ Attr.href (Utils.contentHistory fileMeta.Slug) ]
+                                                      [ Text.raw fileMeta.FormattedModificationTime ] ] ] ] ] ]
+                    Elem.main [] [ Text.raw content ] ]
+              Elem.script [ Attr.src "scripts/prism.js" ] [] ]
 
 module ApiResponse =
 
-    let getHtmlList (limit: int) (fileMetas: FileMeta seq) =
+    let getHtmlList (limit: int) (fileMetas: FileMeta seq) : string seq =
         fileMetas
         |> Utils.limitFileMetas limit
         |> Seq.map (fun (fileMeta: FileMeta) -> fileMeta |> HtmlView.postCard |> renderHtml)
 
+    let getBySlug (slug: string) (fileMetas: FileMeta seq) : FileMeta option =
+        fileMetas |> Seq.tryFind (fun (fileMeta: FileMeta) -> fileMeta.Slug = slug)
 
-    let getHtmlBySlug (fileMetas: FileMeta seq) (slug: string) : Async<string> =
-        async {
-            match fileMetas |> Seq.tryFind (fun (fileMeta: FileMeta) -> fileMeta.Slug = slug) with
-            | Some(fileMeta: FileMeta) -> return! Content.getHtml fileMeta
-            | None -> return ""
-        }
+    let getBySlugOrDefault<'T> (somer: FileMeta -> 'T) (none: 'T) (slug: string) (fileMetas: FileMeta seq) : 'T =
+        match getBySlug slug fileMetas with
+        | Some(fileMeta: FileMeta) -> somer fileMeta
+        | None -> none
+
+    let getHtmlBySlug (slug: string) (fileMetas: FileMeta seq) : string =
+        getBySlugOrDefault Content.getHtml "" slug fileMetas
 
     let getJsonList (limit: int) (fileMetas: FileMeta seq) : FileResponse seq =
         fileMetas
@@ -233,15 +313,11 @@ module ApiResponse =
         |> Seq.map (fun (fileMeta: FileMeta) ->
             { FileResponse.Slug = fileMeta.Slug
               FileResponse.CreationTime = fileMeta.CreationTime
-              FileResponse.ModificationType = fileMeta.ModificationType
+              FileResponse.ModificationTime = fileMeta.ModificationTime
               FileResponse.Mattr = fileMeta.Mattr })
 
-    let getJsonBySlug (fileMetas: FileMeta seq) (slug: string) : Async<obj> =
-        async {
-            match fileMetas |> Seq.tryFind (fun (fileMeta: FileMeta) -> fileMeta.Slug = slug) with
-            | Some(fileMeta: FileMeta) -> return! Content.getJson fileMeta
-            | None -> return Map.empty
-        }
+    let getJsonBySlug (slug: string) (fileMetas: FileMeta seq) : obj =
+        getBySlugOrDefault Content.getJson "" slug fileMetas
 
 module ApiHandler =
     let htmlListHandler (fileMetas: FileMeta seq) : HttpHandler =
@@ -258,7 +334,7 @@ module ApiHandler =
             task {
                 let route: RouteCollectionReader = Request.getRoute ctx
                 let slug: string = route.GetString "slug"
-                let! (htmlResponse: string) = ApiResponse.getHtmlBySlug fileMetas slug
+                let htmlResponse: string = ApiResponse.getHtmlBySlug slug fileMetas
                 return! Response.ofHtmlString htmlResponse ctx
             }
 
@@ -276,20 +352,26 @@ module ApiHandler =
             task {
                 let route: RouteCollectionReader = Request.getRoute ctx
                 let slug: string = route.GetString "slug"
-                let! (jsonResponse: obj) = ApiResponse.getJsonBySlug fileMetas slug
+                let jsonResponse: obj = ApiResponse.getJsonBySlug slug fileMetas
                 return! Response.ofJson jsonResponse ctx
             }
 
     let mainPageHandler (fileMetas: FileMeta seq) : HttpHandler =
-        fileMetas |> Utils.limitFileMetas 5 |> HtmlView.homeView |> Response.ofHtml
+        fileMetas |> Utils.limitFileMetas 0 |> HtmlView.homeView |> Response.ofHtml
 
     let detailPage (fileMetas: FileMeta seq) : HttpHandler =
         fun (ctx: HttpContext) ->
             task {
                 let route: RouteCollectionReader = Request.getRoute ctx
                 let slug: string = route.GetString "slug"
-                let! (htmlResponse: string) = ApiResponse.getHtmlBySlug fileMetas slug
-                return! Response.ofHtml (HtmlView.detailView htmlResponse) ctx
+
+                let response =
+                    match ApiResponse.getBySlug slug fileMetas with
+                    | None -> Response.ofPlainText "Not Found" ctx
+                    | Some(fileMeta: FileMeta) ->
+                        Response.ofHtml (HtmlView.detailView (Content.getHtml fileMeta) fileMeta) ctx
+
+                return! response
             }
 
 let (fileMetas: FileMeta seq) =
--- content/love-at-first-sight-with-f#.md
+++ content/love-at-first-sight-with-f#.md
@@ -3,11 +3,11 @@ title: Love at First Sight with F#
 description: A story about the first time when I meet F#.
 ---
 
-# TODO
+## TODO
 
-- First time hear about F#
-- SAFE stack
-- Experience with SAFE stack (docs, learning, debugging, searching for resource, error)
-- Though about SAFE stack
-- Migrate to Giraffe
-- Next step
+- [ ] First time hear about F#
+- [ ] SAFE stack
+- [ ] Experience with SAFE stack (docs, learning, debugging, searching for resource, error)
+- [ ] Though about SAFE stack
+- [ ] Migrate to Giraffe
+- [ ] Next step
